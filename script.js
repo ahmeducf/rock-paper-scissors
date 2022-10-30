b4112b0d0256f3d7b687887c8dba3700bae07fa5
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2022-10-30 03:07:07 +0200

    Add event Listeners to weapons buttons

--- script.js
+++ script.js
@@ -1,73 +1,123 @@
-const ROCK = "rock";
-const PAPER = "paper";
-const SCISSORS = "scissors";
+const ROCK_CHOICE = "rock";
+const PAPER_CHOICE = "paper";
+const SCISSORS_CHOICE = "scissors";
 
-const WIN = "win";
-const LOSE = "lose";
+const PLAYER = "player";
+const COMPUTER = "computer";
 const TIE = "tie";
 
+const POINTS_NEEDED_TO_WIN = 5;
+
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice() {
+  let choices = [ROCK_CHOICE, PAPER_CHOICE, SCISSORS_CHOICE];
   let choice = Math.floor((Math.random() * 100) % 3);
-  let choices = [ROCK, PAPER, SCISSORS];
 
   return choices[choice];
 }
 
-function getPlayerChoice() {
-  let choice = prompt("choose between Rock, Paper, or Scissors");
-  choice = choice.toLowerCase();
-
-  while (choice !== ROCK && choice !== PAPER && choice !== SCISSORS) {
-    choice = prompt("Wrong Choice!! choose between Rock, Paper, or Scissors");
-    choice = choice.toLowerCase();
-  }
-
-  return choice;
-}
-
-// playRound function gets player and computer selections and returns
-// whether the player win, lose or the game resulted in a tie.
-function playRound(playerSelection, computerSelection) {
-  let playerRoundResult = LOSE;
-
+function determineWinner(playerSelection, computerSelection) {
+  let roundWinner = COMPUTER
   if (playerSelection === computerSelection) {
-    playerRoundResult = TIE;
+    roundWinner = TIE;
   } else if (
-    (playerSelection === PAPER && computerSelection === ROCK) ||
-    (playerSelection === ROCK && computerSelection === SCISSORS) ||
-    (playerSelection === SCISSORS && computerSelection === PAPER)
+    (playerSelection === PAPER_CHOICE && computerSelection === ROCK_CHOICE) ||
+    (playerSelection === ROCK_CHOICE && computerSelection === SCISSORS_CHOICE) ||
+    (playerSelection === SCISSORS_CHOICE && computerSelection === PAPER_CHOICE)
   ) {
-    playerRoundResult = WIN;
+    roundWinner = PLAYER;
   }
 
-  return playerRoundResult;
+  return roundWinner;
 }
 
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
+function updateGameScore() {
+  const playerScoreDiv = document.querySelector(`.player[data-player="user"] .total-game-points`);
+  const computerScoreDiv = document.querySelector(`.player[data-player="computer"] .total-game-points`);
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-
-        let roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult === WIN) {
-            playerPoints++;
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        } else if (roundResult === LOSE) {
-            computerPoints++;
-            console.log(`You Lose! ${playerSelection} can't beat ${computerSelection}`);
-        } else {
-            console.log(`This Round is Tie!`);
-        }
-    }
-
-    if (playerPoints > computerPoints) {
-        console.log(`%c Congratulations, You Won the Game!!`, 'font-size: 24px; background: gold');
-    } else if (playerPoints < computerPoints) {
-        console.log(`%c Unfortunately, You Lost the Game!! :(`, 'font-size: 24px; background: red');
-    } else {
-        console.log(`%c The Game Ended up in Tie!!`, 'font-size: 24px; background: gray');
-    }
+  playerScoreDiv.innerText = `Player: ${playerPoints}`;
+  computerScoreDiv.innerText = `Computer: ${computerPoints}`;
 }
+
+function updateRoundChoices(playerSelection, computerSelection) {
+  const choices = new Map();
+  choices.set(ROCK_CHOICE, "✊");
+  choices.set(PAPER_CHOICE, "✋");
+  choices.set(SCISSORS_CHOICE, "✌");
+ 
+  const playerChoiceDiv = document.querySelector(`.player[data-player="user"] .choice`);
+  const computerChoiceDiv = document.querySelector(`.player[data-player="computer"] .choice`);
+
+  playerChoiceDiv.innerText = choices.get(playerSelection);
+  computerChoiceDiv.innerText = choices.get(computerSelection);
+}
+
+function updateRunningRoundMessage(roundWinner, playerSelection, computerSelection) {
+  const h2 = document.querySelector(`.title h2`);
+  const p = document.querySelector(`.title p`);
+  if (roundWinner === TIE) {
+    h2.innerText = `It's a tie!`;
+    p.innerText = `${playerSelection} ties with ${computerSelection}`;
+  } else if (roundWinner === PLAYER) {
+    h2.innerText = `You won!`;
+    p.innerText = `${playerSelection} beats ${computerSelection}`;
+  } else {
+    h2.innerText = `You lost!`;
+    p.innerText = `${playerSelection} is beaten by ${computerSelection}`;
+  }
+}
+
+function endGame(roundWinner) {
+  const h2 = document.querySelector(`.title h2`);
+  const p = document.querySelector(`.title p`);
+
+  if (roundWinner === PLAYER) {
+    h2.innerText = `You Won the Game!`;
+  } else {
+    h2.innerText = `You Lost the Game!`;
+  }
+
+  p.remove();
+  playAgainButton.classList.add('display-button');
+}
+
+function playRound(e) {
+  if (playerPoints >= POINTS_NEEDED_TO_WIN ||
+    computerPoints >= POINTS_NEEDED_TO_WIN) {
+      return;
+  }
+
+  const computerSelection = getComputerChoice();
+  const playerSelection = e.target.getAttribute('data-weapon');  
+  const roundWinner = determineWinner(playerSelection, computerSelection);
+
+  updateRoundChoices(playerSelection, computerSelection);
+
+  if (roundWinner === PLAYER) {
+    playerPoints++;
+    updateGameScore();
+
+    if (playerPoints >= POINTS_NEEDED_TO_WIN) {
+        endGame(roundWinner);
+        return;
+    }
+  } else if (roundWinner === COMPUTER) {
+    computerPoints++;
+    updateGameScore();
+
+    if (computerPoints >= POINTS_NEEDED_TO_WIN) {
+      endGame();
+      return;
+    }
+  }
+
+  updateRunningRoundMessage(roundWinner, playerSelection, computerSelection);
+}
+
+const buttons = document.querySelectorAll(`.weapon`);
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+const playAgainButton = document.querySelector(`.title button`);
+playAgainButton.addEventListener('click', () => window.location.reload());
